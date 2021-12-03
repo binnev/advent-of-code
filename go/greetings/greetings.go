d340de32370b91382884c507604fe9a6b35b6f08
binnev <byntje@gmail.com>
2021-12-03 12:18:08 +0100

    random greeting

--- go/greetings/greetings.go
+++ go/greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person
@@ -13,10 +15,28 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	// Return a greeting that embeds the name in the message
 	// message := ... is shorthand for declaring and initialising a variable in one line:
 	// var message
 	// message = ...
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name) // I guess this is like str.format in python
 	return message, nil
+}
+
+// init sets initial values for variables used in the function
+// go auto-executes init functions at the program startup, after global variables have been initialised!
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned message is selected at random
+func randomFormat() string {
+	// a slice of message formats -- I think this is equivalent to a python list
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// return a randomly selected message format by specifying a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
