901f7995608ad09f86ed1a8e3864e4f7cb6a3920
binnev <byntje@gmail.com>
2021-12-10 08:53:01 +0100

    day10 part 2!

--- python/day10.py
+++ python/day10.py
@@ -1,13 +1,4 @@
-raw = """[({(<(())[]>[[{[]{<()<>>
-[(()[<>])]({[<{<<[]>>(
-{([(<{}[<>[]}>{[]{[(<()>
-(((({<>}<{<{<>}{[]{[]{}
-[[<[([]))<([[{}[[()]]]
-[{[{({}]{}}([{[{{{}}([]
-{<[[]]>}<{[{[{[]{()[[[]
-[<(<(<(<{}))><([]([]()
-<{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]"""
+import numpy
 
 raw = """(<[<[((<[<([<<[]()>{{}()}>({<>[]}<[][]>)])<<[{{}[]}[{}<>]]([()<>][()()])>>>]((<{{([]{})<[]<>>}(<[
 <[{({[[<{[{{{<<><>><{}>}<([]()){()[]}>}({(()()){{}<>}})}{([<()()>[<><>]><<{}[]>({}<>)>)}]{{[((<>()){<>[]
@@ -104,6 +95,17 @@ raw = """(<[<[((<[<([<<[]()>{{}()}>({<>[]}<[][]>)])<<[{{}[]}[{}<>]]([()<>][()()]
 ([(({<([{({<(([]<>)(()[])){<()()>{<>()}}><([[][]]<()<>>)[[[][]]<()<>>]>}[{[{()[]}(<>[])]([<>[]]<<>()>)}])
 {{{([<[([{(<[{[]()}<[]{}>]>)}])<<<<({(<>())<{}()>})[{({}[]}{<>[]}}<<[]<>><{}[]>>]><(<[()<>"""
 
+# raw = """[({(<(())[]>[[{[]{<()<>>
+# [(()[<>])]({[<{<<[]>>(
+# {([(<{}[<>[]}>{[]{[(<()>
+# (((({<>}<{<{<>}{[]{[]{}
+# [[<[([]))<([[{}[[()]]]
+# [{[{({}]{}}([{[{{{}}([]
+# {<[[]]>}<{[{[{[]{()[[[]
+# [<(<(<(<{}))><([]([]()
+# <{([([[(<>()){}]>(<<{{
+# <{([{{}}[<[[[<>{}]]]>[]]"""
+
 
 def init():
     return raw.splitlines()
@@ -126,8 +128,15 @@ illegal_chars = {
     ">": 25137,
 }
 
+completion_scores = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
 
-def check_line(line):
+
+def check_line1(line):
     stack = []
     for char in line:
         if char in openers:
@@ -135,27 +144,66 @@ def check_line(line):
         else:
             expected_opener = reverse_mapping[char]
             if stack[-1] != expected_opener:
-                print(f"line {line}: expected {mapping[stack[-1]]}, but found {char} instead")
+                # print(f"line {line}: expected {mapping[stack[-1]]}, but found {char} instead")
                 return illegal_chars[char]
             else:
                 stack.pop()
     if len(stack) == 0:
         return 0
     else:
-        print(f"Chars still on stack: {stack}")
+        # print(f"Chars still on stack: {stack}")
         return 0
+
+
+def check_line2(line):
+    stack = []
+    for char in line:
+        if char in openers:
+            stack.append(char)
+        else:
+            expected_opener = reverse_mapping[char]
+            if stack[-1] != expected_opener:
+                return 0
+            else:
+                stack.pop()
+    if len(stack) == 0:
+        return 0
+    else:
+        remaining_chars = []
+        while stack:
+            char = stack.pop()
+            remaining_chars.append(mapping[char])
+        return remaining_chars
 
 
 def part1():
     input = init()
     score = 0
     for line in input:
-        score += check_line(line)
+        score += check_line1(line)
+    return score
+
+
+def calculate_score(chars):
+    score = 0
+    for char in chars:
+        char_score = completion_scores[char]
+        score *= 5
+        score += char_score
     return score
 
 
 def part2():
-    pass
+    input = init()
+    scores = []
+    for line in input:
+        remaining_chars = check_line2(line)
+        if not remaining_chars:
+            continue
+        s = calculate_score(remaining_chars)
+        print(line, s)
+        scores.append(s)
+    return int(numpy.median(scores))
 
 
 if __name__ == "__main__":
