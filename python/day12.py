32d3a914abbecec264cf2bb5e4b5fc1b1be0dd41
binnev <byntje@gmail.com>
2021-12-12 09:50:06 +0100

    day 12 part 2

--- python/day12.py
+++ python/day12.py
@@ -1,55 +1,34 @@
-raw = """start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end"""
+raw = """YW-end
+DK-la
+la-XG
+end-gy
+zq-ci
+XG-gz
+TF-la
+xm-la
+gy-gz
+ci-start
+YW-ci
+TF-zq
+ci-DK
+la-TS
+zq-YW
+gz-YW
+zq-gz
+end-gz
+ci-TF
+DK-zq
+gy-YW
+start-DK
+gz-DK
+zq-la
+start-TF"""
 
-# raw= """dc-end
-# HN-start
-# start-kj
-# dc-start
-# dc-HN
-# LN-dc
-# HN-end
-# kj-sa
-# kj-HN
-# kj-dc
-# """
-#
-# raw = """YW-end
-# DK-la
-# la-XG
-# end-gy
-# zq-ci
-# XG-gz
-# TF-la
-# xm-la
-# gy-gz
-# ci-start
-# YW-ci
-# TF-zq
-# ci-DK
-# la-TS
-# zq-YW
-# gz-YW
-# zq-gz
-# end-gz
-# ci-TF
-# DK-zq
-# gy-YW
-# start-DK
-# gz-DK
-# zq-la
-# start-TF"""
 
 links = dict()
 for node1, node2 in [row.split("-") for row in raw.splitlines()]:
     links.setdefault(node1, []).append(node2)
     links.setdefault(node2, []).append(node1)
-
-
 
 
 def dfs(path):
@@ -68,16 +47,22 @@ def dfs(path):
         foo = dfs(path + [neighbour])
         new_paths.extend(foo)
 
+    return new_paths
+
+
 def dfs2(path):
     node = path[-1]
     new_paths = []
     for neighbour in links[node]:
-        if neighbour.islower() and path.count(neighbour) >= 2:
+        if neighbour == "start":
             continue
-
         # base case -- finish
         if neighbour == "end":
             new_paths.append(path + [neighbour])
+            continue
+
+        # if neighbour.islower() and path.count(neighbour) >= 2:
+        if neighbour.islower() and not check_small_cave_visits(path + [neighbour]):
             continue
 
         # recursive case
@@ -89,15 +74,19 @@ def dfs2(path):
 
 def part1():
     paths = dfs(["start"])
-    # for path in paths:
-    #     print(",".join(path))
     return len(paths)
+
+
+def check_small_cave_visits(path):
+    counts = {node: path.count(node) for node in set(path) if node.islower()}
+    if max(counts.values()) > 2:
+        return False
+    gt1 = list(filter(lambda k: counts[k] > 1, counts))
+    return len(gt1) < 2
 
 
 def part2():
     paths = dfs2(["start"])
-    for path in paths:
-        print(",".join(path))
     return len(paths)
 
 
