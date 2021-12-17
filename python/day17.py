9ecccb91f1314226aa1a46704905db786fc1bee1
binnev <byntje@gmail.com>
2021-12-17 10:00:36 +0100

    wip

--- python/day17.py
+++ python/day17.py
@@ -60,34 +60,56 @@ def part1():
     things, target_x, target_y = init()
     things[(0, 0)] = "S"
     # 1) calibrate x
-    hit = False
+    u0_range = []
     u0 = 1
-    v0 = 1
-    while not hit:
-        print(f"trying u0={u0}, v0=0")
-        positions, _ = calc_traj(u0, v0, things)
+    while True:
+        positions, _ = calc_traj(u0, 0, things)
         final_pos = positions[-1]
-        hit = final_pos[0] in target_x
-        for p in positions:
-            things[p] = "#"
-        if hit: break
+        if final_pos[0] in target_x:
+            u0_range.append(u0)
+            print(f"u0 lower bound = {u0}")
+            break
         u0 += 1
-        v0 += 1
 
-    hit = False
+    while True:
+        positions, _ = calc_traj(u0, 0, things)
+        final_pos = positions[-1]
+        if final_pos[0] not in target_x:
+            print(f"u0 lower bound = {u0-1}")
+            break
+        u0_range.append(u0)
+        u0 += 1
+
+    # 1) calibrate y
+    v0_range = []
     v0 = 1
     while True:
-        print(f"trying u0={u0}, v0={v0+1}")
-        positions, hit = calc_traj(u0, v0 + 1, things)
-        for p in positions:
-            things[p] = "#"
-        if not hit:
-            break  # v0 was the last one to hit
+        positions, _ = calc_traj(0, v0, things)
+        final_pos = positions[-2]
+        if final_pos[1] in target_y:
+            v0_range.append(v0)
+            print(f"v0 lower bound = {v0}")
+            break
         v0 += 1
 
-    # print_stuff(things)
-    max_y = max([y for x, y in positions])
-    return max_y
+    while True:
+        positions, _ = calc_traj(0, v0, things)
+        final_pos = positions[-2]
+        if final_pos[1] not in target_y:
+            print(f"v0 lower bound = {v0-1}")
+            break
+        v0_range.append(v0)
+        v0 += 1
+
+    trajectories = []
+    for u0 in u0_range:
+        for v0 in v0_range:
+            positions, hit = calc_traj(u0, v0, things)
+            if hit:
+                trajectories.append(positions)
+
+    return max(y for traj in trajectories for x, y in traj)
+"""New idea: get all trajectories that _cross_ the target (including diagonally) """
 
 
 if __name__ == "__main__":
