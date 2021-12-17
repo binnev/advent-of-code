478359c9b2f18bc9bff2326a7718bc36e759c4ec
binnev <byntje@gmail.com>
2021-12-17 10:33:23 +0100

    brute forced part 1 -.-

--- python/day17.py
+++ python/day17.py
@@ -2,7 +2,7 @@ raw = "target area: x=143..177, y=-106..-71"
 dummy = "target area: x=20..30, y=-10..-5"
 
 
-raw = dummy
+# raw = dummy
 
 
 def init():
@@ -59,51 +59,51 @@ def part1():
     """
     things, target_x, target_y = init()
     things[(0, 0)] = "S"
-    # 1) calibrate x
-    u0_range = []
-    u0 = 1
-    while True:
-        positions, _ = calc_traj(u0, 0, things)
-        final_pos = positions[-1]
-        if final_pos[0] in target_x:
-            u0_range.append(u0)
-            print(f"u0 lower bound = {u0}")
-            break
-        u0 += 1
-
-    while True:
-        positions, _ = calc_traj(u0, 0, things)
-        final_pos = positions[-1]
-        if final_pos[0] not in target_x:
-            print(f"u0 lower bound = {u0-1}")
-            break
-        u0_range.append(u0)
-        u0 += 1
-
-    # 1) calibrate y
-    v0_range = []
-    v0 = 1
-    while True:
-        positions, _ = calc_traj(0, v0, things)
-        final_pos = positions[-2]
-        if final_pos[1] in target_y:
-            v0_range.append(v0)
-            print(f"v0 lower bound = {v0}")
-            break
-        v0 += 1
-
-    while True:
-        positions, _ = calc_traj(0, v0, things)
-        final_pos = positions[-2]
-        if final_pos[1] not in target_y:
-            print(f"v0 lower bound = {v0-1}")
-            break
-        v0_range.append(v0)
-        v0 += 1
+    # # 1) calibrate x
+    # u0_range = []
+    # u0 = 1
+    # while True:
+    #     positions, _ = calc_traj(u0, 0, things)
+    #     final_pos = positions[-1]
+    #     if final_pos[0] in target_x:
+    #         u0_range.append(u0)
+    #         print(f"u0 lower bound = {u0}")
+    #         break
+    #     u0 += 1
+    #
+    # while True:
+    #     positions, _ = calc_traj(u0, 0, things)
+    #     final_pos = positions[-1]
+    #     if final_pos[0] not in target_x:
+    #         print(f"u0 lower bound = {u0-1}")
+    #         break
+    #     u0_range.append(u0)
+    #     u0 += 1
+    #
+    # # 1) calibrate y
+    # v0_range = []
+    # v0 = 1
+    # while True:
+    #     positions, _ = calc_traj(0, v0, things)
+    #     final_pos = positions[-2]
+    #     if final_pos[1] in target_y:
+    #         v0_range.append(v0)
+    #         print(f"v0 lower bound = {v0}")
+    #         break
+    #     v0 += 1
+    #
+    # while True:
+    #     positions, _ = calc_traj(0, v0, things)
+    #     final_pos = positions[-2]
+    #     if final_pos[1] not in target_y:
+    #         print(f"v0 lower bound = {v0-1}")
+    #         break
+    #     v0_range.append(v0)
+    #     v0 += 1
 
     trajectories = []
-    for u0 in u0_range:
-        for v0 in v0_range:
+    for u0 in range(200):
+        for v0 in range(200):
             positions, hit = calc_traj(u0, v0, things)
             if hit:
                 trajectories.append(positions)
