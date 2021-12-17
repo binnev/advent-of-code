44a7fded8a2948ae2ec2ece08f8a145e3f13f2c3
binnev <byntje@gmail.com>
2021-12-17 10:56:29 +0100

    fiddling

--- python/day17.py
+++ python/day17.py
@@ -31,11 +31,10 @@ def print_stuff(things):
         print("")
 
 
-def calc_traj(u, v, things):
+def calc_traj(u, v, target_x, target_y):
     x = 0
     y = 0
     positions = []
-    min_y = min(y for (x, y), code in things.items() if code == "T")
     while True:
         x += u
         y += v
@@ -43,9 +42,9 @@ def calc_traj(u, v, things):
         if u != 0:
             u += -1 if u > 0 else 1
         v -= 1
-        if things.get((x, y)) == "T":
+        if x in target_x and y in target_y:
             return positions, True
-        if y < min_y:
+        if y < target_y.start:
             return positions, False
 
 
@@ -59,52 +58,11 @@ def part1():
     """
     things, target_x, target_y = init()
     things[(0, 0)] = "S"
-    # # 1) calibrate x
-    # u0_range = []
-    # u0 = 1
-    # while True:
-    #     positions, _ = calc_traj(u0, 0, things)
-    #     final_pos = positions[-1]
-    #     if final_pos[0] in target_x:
-    #         u0_range.append(u0)
-    #         print(f"u0 lower bound = {u0}")
-    #         break
-    #     u0 += 1
-    #
-    # while True:
-    #     positions, _ = calc_traj(u0, 0, things)
-    #     final_pos = positions[-1]
-    #     if final_pos[0] not in target_x:
-    #         print(f"u0 lower bound = {u0-1}")
-    #         break
-    #     u0_range.append(u0)
-    #     u0 += 1
-    #
-    # # 1) calibrate y
-    # v0_range = []
-    # v0 = 1
-    # while True:
-    #     positions, _ = calc_traj(0, v0, things)
-    #     final_pos = positions[-2]
-    #     if final_pos[1] in target_y:
-    #         v0_range.append(v0)
-    #         print(f"v0 lower bound = {v0}")
-    #         break
-    #     v0 += 1
-    #
-    # while True:
-    #     positions, _ = calc_traj(0, v0, things)
-    #     final_pos = positions[-2]
-    #     if final_pos[1] not in target_y:
-    #         print(f"v0 lower bound = {v0-1}")
-    #         break
-    #     v0_range.append(v0)
-    #     v0 += 1
 
     trajectories = []
     for u0 in range(250):
         for v0 in range(-150, 250):
-            positions, hit = calc_traj(u0, v0, things)
+            positions, hit = calc_traj(u0, v0, target_x, target_y)
             if hit:
                 trajectories.append(positions)
 
