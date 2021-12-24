96363ec10d369c82fa32590e0de01d914e845b71
binnev <byntje@gmail.com>
2021-12-24 09:01:48 +0100

    ALU working (I think?)

---
+++ python/day24.py
@@ -0,0 +1,335 @@
+raw = """inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 13
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 10
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 3
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 1
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 9
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -4
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 3
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 12
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 5
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 12
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 1
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 0
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -2
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 13
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -5
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 7
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 15
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 12
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 8
+mul y x
+add z y"""
+
+
+def init(raw_string):
+    instructions = []
+    for line in raw_string.splitlines():
+        operation, *terms = line.split(" ")
+        instructions.append((operation, terms))
+    return instructions
+
+
+class Alu:
+    w: int = 0
+    x: int = 0
+    y: int = 0
+    z: int = 0
+
+    def parse(self, b):
+        return int(b) if b.isdigit() else getattr(self, b)
+
+    def inp(self, a: str, b: str):
+        b = self.parse(b)
+        setattr(self, a, b)
+
+    def mul(self, a, b):
+        b = self.parse(b)
+        a_value = getattr(self, a)
+        result = a_value * b
+        setattr(self, a, result)
+
+    def add(self, a, b):
+        b = self.parse(b)
+        a_value = getattr(self, a)
+        result = a_value + b
+        setattr(self, a, result)
+
+    def div(self, a, b):
+        b = self.parse(b)
+        a_value = getattr(self, a)
+        result = a_value // b
+        setattr(self, a, result)
+
+    def mod(self, a, b):
+        b = self.parse(b)
+        a_value = getattr(self, a)
+        result = a_value % b
+        setattr(self, a, result)
+
+    def eql(self, a, b):
+        b = self.parse(b)
+        a_value = getattr(self, a)
+        result = int(a_value == b)
+        setattr(self, a, result)
+
+    def __repr__(self):
+        w = self.w
+        x = self.x
+        y = self.y
+        z = self.z
+        return f"ALU({w=}, {x=}, {y=}, {z=})"
+
+
+def part1():
+    alu = Alu()
+    alu.inp("w", "4")
+    alu.add("w", "3")
+    alu.mul("w", "3")
+    alu.div("w", "2")
+    alu.mod("w", "3")
+    alu.eql("w", "1")
+    # alu.div("w", "2")
+    # alu.add("y", "w")
+    # alu.mod("y", "2")
+    # alu.div("w", "2")
+    # alu.add("x", "w")
+    # alu.mod("x", "2")
+    # alu.div("w", "2")
+    # alu.mod("w", "2")
+    print(alu)
+
+
+if __name__ == "__main__":
+    p1 = part1()
+    print(f"{p1=}")
