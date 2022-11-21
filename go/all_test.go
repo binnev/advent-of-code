b2cec91035a358bf99fbd2ede4ade2537d1abe11
binnev <byntje@gmail.com>
2022-11-21 15:06:08 +0100

    Merged test files into one. Updated gitignore

--- go/all_test.go
+++ go/all_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent/_2020"
+	"advent/_2021"
 	"advent/utils"
 	"testing"
 )
@@ -15,6 +16,30 @@ func Test2020(t *testing.T) {
 		{_2020.Day1Part2, "69596112"},
 		{_2020.Day2Part1, "628"},
 		{_2020.Day2Part2, "705"},
+	}
+
+	for _, tc := range cases {
+		t.Run(utils.GetFuncName(tc.f), func(t *testing.T) {
+			result := tc.f()
+			if result != tc.expected {
+				t.Fatalf("%v failed; got %v; expected %v",
+					utils.GetFuncName(tc.f),
+					result,
+					tc.expected,
+				)
+			}
+		})
+
+	}
+}
+
+func Test2021(t *testing.T) {
+	cases := []struct {
+		f        func() string
+		expected string
+	}{
+		{_2021.Day1Part1, "1482"},
+		{_2021.Day1Part2, "1518"},
 	}
 
 	for _, tc := range cases {
