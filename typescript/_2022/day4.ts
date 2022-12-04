9c1516aecb6c4aa527329fba2d05d4a916038d64
binnev <byntje@gmail.com>
2022-12-04 09:47:56 +0100

    refactoring

--- typescript/_2022/day4.ts
+++ typescript/_2022/day4.ts
@@ -1,6 +1,9 @@
 import {loadPuzzleInput, profile} from "../utils";
 
-function parseInput(): number[][][] {
+type ElfRange = number[]
+type Elves = ElfRange[]
+
+function parseInput(): Elves[] {
     return loadPuzzleInput("2022/day4")
         .split("\n")
         .map(row => row
@@ -12,24 +15,20 @@ function parseInput(): number[][][] {
         )
 }
 
-function contains([s1, e1]: number[], [s2, e2]: number[]): boolean {
+function contains([s1, e1]: ElfRange, [s2, e2]: ElfRange): boolean {
     return (s2 >= s1 && e2 <= e1) || (s1 >= s2 && e1 <= e2)
 }
 
-function overlaps([s1, e1]: number[], [s2, e2]: number[]): boolean {
+function overlaps([s1, e1]: ElfRange, [s2, e2]: ElfRange): boolean {
     return (e1 >= s2 && e2 >= s1) || (e2 >= s1 && e1 >= s2)
 }
 
 export function day4Part1(): number {
-    return parseInput()
-        .map(([elf1, elf2]) => contains(elf1, elf2) ? 1 : 0)
-        .reduce((a, b) => a + b, 0)
+    return parseInput().filter(([elf1, elf2]) => contains(elf1, elf2)).length
 }
 
 export function day4Part2(): number {
-    return parseInput()
-        .map(([elf1, elf2]) => overlaps(elf1, elf2) ? 1 : 0)
-        .reduce((a, b) => a + b, 0)
+    return parseInput().filter(([elf1, elf2]) => overlaps(elf1, elf2)).length
 }
 
 if (require.main === module) {
