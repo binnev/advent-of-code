499efa99855a794d1b7a83d3e25d526402ae8a0e
binnev <byntje@gmail.com>
2022-12-07 22:09:05 +0100

    got it working with alternate string representation

--- python/_2022/day7.py
+++ python/_2022/day7.py
@@ -25,88 +25,64 @@ $ ls
 7214296 k"""
 
 
-class Folder(dict):
-    parent: "Folder|None"
-
-    def __init__(self, parent, *args, **kwargs):
-        self.parent = parent
-
-    def get_size(self) -> int:
-        return sum(item.get_size() if isinstance(item, Folder) else item for item in self.values())
-
-
-def explore_folders(input: str) -> Folder:
-    root = cwd = Folder(parent=None)
-    mode = ""
+def explore_folders(input: str) -> dict[str:int]:
+    root = cwd = ""
+    contents = {"/": 0}
     for line in input.split("\n"):
 
-        # handle commands
         if line.startswith("$"):
             _, cmd = line.split(" ", maxsplit=1)
-
             match cmd.split():
                 case ["cd", dir_name]:
                     match dir_name:
                         case "..":
-                            cwd = cwd.parent
+                            cwd, _ = cwd.rsplit("/", maxsplit=1)
                         case "/":
                             cwd = root
                         case _:
-                            cwd = cwd[dir_name]
-                case ["ls"]:
-                    mode = "ls"
+                            cwd = "/".join([cwd, dir_name])
 
-        else:  # not a command
-            if mode == "ls":
-                match line.split():
-                    case ["dir", dir_name]:
-                        cwd[dir_name] = Folder(parent=cwd)
-                    case [file_size, file_name]:
-                        cwd[file_name] = int(file_size)
-    return root
+        else:  # must be lines following "ls"
+            match line.split():
+                case ["dir", dir_name]:
+                    contents["/".join([cwd, dir_name])] = 0
+                case [file_size, file_name]:
+                    contents["/".join([cwd, file_name])] = int(file_size)
+    return contents
 
 
-def find_under_100000(folder: Folder):
-    total_size = 0
-    size = folder.get_size()
-    if size <= 100000:
-        total_size += size
-
-    for name, value in folder.items():
-        if isinstance(value, Folder):
-            total_size += find_under_100000(value)
-
-    return total_size
+def get_folder_size(contents: dict[str:int], folder_path: str) -> int:
+    return sum(
+        size
+        for path, size in contents.items()
+        if path.startswith(folder_path) and not path.endswith(folder_path)
+    )
 
 
 @utils.profile
 def part1() -> int:
     input = utils.load_puzzle_input("2022/day7")
-    root = explore_folders(input)
-    return find_under_100000(root)
-
-
-def all_folder_sizes(folder: Folder) -> list[int]:
-    all_sizes = []
-    all_sizes.append(folder.get_size())
-
-    for name, value in folder.items():
-        if isinstance(value, Folder):
-            all_sizes.extend(all_folder_sizes(value))
-
-    return all_sizes
+    contents = explore_folders(input)
+    folders = {k for k, v in contents.items() if v == 0}
+    total = 0
+    for folder in folders:
+        if (size := get_folder_size(contents, folder)) < 100000:
+            total += size
+    return total
 
 
 @utils.profile
 def part2() -> int:
+    input = utils.load_puzzle_input("2022/day7")
+    contents = explore_folders(input)
     TOTAL_SPACE = 70_000_000
     REQUIRED_SPACE = 30_000_000
-    input = utils.load_puzzle_input("2022/day7")
-    root = explore_folders(input)
-    occupied_space = root.get_size()
+    occupied_space = get_folder_size(contents, folder_path="/")
     unused_space = TOTAL_SPACE - occupied_space
     need_to_delete = REQUIRED_SPACE - unused_space
-    all_sizes = all_folder_sizes(root)
+
+    folders = {k for k, v in contents.items() if v == 0}
+    all_sizes = [get_folder_size(contents, f) for f in folders]
     return min(s for s in all_sizes if s > need_to_delete)
 
 
