8d2bcce39dce7af753f4ed8f6bfcaa5f7d4a1cfd
binnev <byntje@gmail.com>
2022-12-11 10:15:40 +0100

    day11 part 2!

--- python/_2022/day11.py
+++ python/_2022/day11.py
@@ -37,7 +37,7 @@ class Monkey:
     id: int
     inventory: list[int]
     operation: Callable  # changes worry level
-    test: Callable  # checks where it should throw item
+    divisor: int
     if_false: int
     if_true: int
     count: int = 0
@@ -45,46 +45,51 @@ class Monkey:
     def throw(self, monkeys: list["Monkey"]):
         item = self.inventory.pop(0)
         item = self.operation(item)
-        item = item // 3
+        item = self.decrease_worry(item)
         result = self.test(item)
         monkey_id = self.if_true if result is True else self.if_false
-        # worry level decreases
         monkey = next(m for m in monkeys if m.id == monkey_id)
         monkey.inventory.append(item)
         self.count += 1
 
+    def test(self, number: int) -> bool:
+        return number % self.divisor == 0
+
+    def decrease_worry(self, number: int) -> int:
+        return number // 3
+
 
 def example_monkeys():
     return [
-        Monkey(
+        dict(
             id=0,
             inventory=[79, 98],
             operation=lambda worry: worry * 19,
-            test=lambda worry: worry % 23 == 0,
+            divisor=23,
             if_true=2,
             if_false=3,
         ),
-        Monkey(
+        dict(
             id=1,
             inventory=[54, 65, 75, 74],
             operation=lambda worry: worry + 6,
-            test=lambda worry: worry % 19 == 0,
+            divisor=19,
             if_true=2,
             if_false=0,
         ),
-        Monkey(
+        dict(
             id=2,
             inventory=[79, 60, 97],
             operation=lambda worry: worry * worry,
-            test=lambda worry: worry % 13 == 0,
+            divisor=13,
             if_true=1,
             if_false=3,
         ),
-        Monkey(
+        dict(
             id=3,
             inventory=[74],
             operation=lambda worry: worry + 3,
-            test=lambda worry: worry % 17 == 0,
+            divisor=17,
             if_true=0,
             if_false=1,
         ),
@@ -93,67 +98,67 @@ def example_monkeys():
 
 def real_monkeys():
     return [
-        Monkey(
+        dict(
             id=0,
             inventory=[83, 88, 96, 79, 86, 88, 70],
             operation=lambda worry: worry * 5,
-            test=lambda worry: worry % 11 == 0,
+            divisor=11,
             if_true=2,
             if_false=3,
         ),
-        Monkey(
+        dict(
             id=1,
             inventory=[59, 63, 98, 85, 68, 72],
             operation=lambda worry: worry * 11,
-            test=lambda worry: worry % 5 == 0,
+            divisor=5,
             if_true=4,
             if_false=0,
         ),
-        Monkey(
+        dict(
             id=2,
             inventory=[90, 79, 97, 52, 90, 94, 71, 70],
             operation=lambda worry: worry + 2,
-            test=lambda worry: worry % 19 == 0,
+            divisor=19,
             if_true=5,
             if_false=6,
         ),
-        Monkey(
+        dict(
             id=3,
             inventory=[97, 55, 62],
             operation=lambda worry: worry + 5,
-            test=lambda worry: worry % 13 == 0,
+            divisor=13,
             if_true=2,
             if_false=6,
         ),
-        Monkey(
+        dict(
             id=4,
             inventory=[74, 54, 94, 76],
             operation=lambda worry: worry * worry,
-            test=lambda worry: worry % 7 == 0,
+            divisor=7,
             if_true=0,
             if_false=3,
         ),
-        Monkey(
+        dict(
             id=5,
             inventory=[58],
             operation=lambda worry: worry + 4,
-            test=lambda worry: worry % 17 == 0,
+            divisor=17,
             if_true=7,
             if_false=1,
         ),
-        Monkey(
+        dict(
             id=6,
             inventory=[66, 63],
             operation=lambda worry: worry + 6,
-            test=lambda worry: worry % 2 == 0,
+            divisor=2,
             if_true=7,
             if_false=5,
         ),
-        Monkey(
+        dict(
             id=7,
             inventory=[56, 56, 90, 96, 68],
             operation=lambda worry: worry + 7,
-            test=lambda worry: worry % 3 == 0,
+            divisor=3,
             if_true=4,
             if_false=1,
         ),
@@ -162,7 +167,7 @@ def real_monkeys():
 
 @utils.profile
 def part1():
-    monkeys = real_monkeys()
+    monkeys = [Monkey(**data) for data in real_monkeys()]
     for round in range(20):
         for monkey in monkeys:
             while monkey.inventory:
@@ -174,9 +179,28 @@ def part1():
 
 @utils.profile
 def part2():
-    ...
+    monkey_data = real_monkeys()
+    modulus = 1
+    for m in monkey_data:
+        modulus *= m["divisor"]
+
+    @dataclass
+    class Monkey2(Monkey):
+        def decrease_worry(self, number: int) -> int:
+            return number % modulus
+
+    monkeys = [Monkey2(**data) for data in monkey_data]
+    for round in range(10000):
+        for monkey in monkeys:
+            while monkey.inventory:
+                monkey.throw(monkeys)
+
+    for m in monkeys:
+        print(f"Monkey {m.id} inspected items {m.count} times")
+    most_active = sorted(monkeys, key=lambda x: -x.count)
+    return most_active[0].count * most_active[1].count
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    assert part1() == 64032
+    assert part2() == 12729522272
