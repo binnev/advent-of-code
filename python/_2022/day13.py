2f54594f03cc1c42cd43ab2c2c0bb6399496ff12
binnev <byntje@gmail.com>
2023-01-06 19:11:33 +0100

    python day13 no longer dependent on eval.

--- python/_2022/day13.py
+++ python/_2022/day13.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from python import utils
 
 example = """[1,1,3,1,1]
@@ -33,63 +35,116 @@ def parse_input(input: str) -> list[tuple[str, str]]:
     return output
 
 
-def are_integers_ordered(left: int, right: int) -> bool | None:
-    if left < right:
-        return True
-    elif left > right:
-        return False
-    else:
-        return None
+def get_list(string: str, start: int) -> tuple[str, int]:
+    depth = 1
+    ii = start + 1
+    while depth:
+        char = string[ii]
+        match char:
+            case "[":
+                depth += 1
+            case "]":
+                depth -= 1
+        ii += 1
+    result = string[start:ii]
+    return result, ii
 
 
-def are_lists_ordered(left: list, right: list) -> bool | None:
-    for a, b in zip(left, right):
-        ordered = are_packets_ordered(a, b)
-        if ordered is not None:
-            return ordered
-
-    # if we reached the end, one of the lists ran out of items: if the left one is shorter,
-    # they're ordered
-    if len(left) < len(right):
-        return True
-    elif len(left) > len(right):
-        return False
-    else:
-        return None  # same order
+def get_number(string: str, start: int) -> tuple[str, int]:
+    ii = start
+    while ii < len(string):
+        char = string[ii]
+        if not char.isnumeric():
+            break
+        ii += 1
+    result = string[start:ii]
+    return result, ii
 
 
-def are_packets_ordered(left: int | list, right: int | list) -> bool:
-    if isinstance(left, int) and isinstance(right, int):
-        return are_integers_ordered(left, right)
-    elif isinstance(left, list) and isinstance(right, list):
-        return are_lists_ordered(left, right)
-    elif isinstance(left, int) and isinstance(right, list):
-        return are_lists_ordered([left], right)
-    elif isinstance(left, list) and isinstance(right, int):
-        return are_lists_ordered(left, [right])
-    else:
-        raise Exception(f"Not sure how to parse {left=}, {right=}")
+def get_values(string: str) -> list[str]:
+    string = string[1:-1]  # string always starts / ends with [ / ]
+    values = list[str]()
+    ii = 0
+    while ii < len(string):
+        char = string[ii]
+        if char.isnumeric():
+            value, ii = get_number(string, ii)
+            values.append(value)
+        elif char == "[":
+            value, ii = get_list(string, ii)
+            values.append(value)
+        else:
+            ii += 1
+    return values
+
+
+def are_packets_ordered(left: str, right: str, depth=0) -> bool | None:
+    indent = "\t" * depth
+    # print(f"{indent}Compare {left} vs {right}")
+
+    # both ints
+    if left.isnumeric() and right.isnumeric():
+        if int(left) < int(right):
+            # print(f"{indent}\tLeft side is smaller, so inputs are in the right order")
+            return True
+        if int(left) == int(right):
+            return None
+        else:
+            # print(f"{indent}\tRight side is smaller, so inputs are not in the right order")
+            return False  # right integer is larger
+
+    # mixed types
+    if left.isnumeric() and not right.isnumeric():
+        converted = f"[{left}]"
+        # print(f"{indent}\tMixed types; convert left to {converted} and retry comparison")
+        return are_packets_ordered(converted, right, depth + 1)
+    if right.isnumeric() and not left.isnumeric():
+        converted = f"[{right}]"
+        # print(f"{indent}\tMixed types; convert right to {converted} and retry comparison")
+        return are_packets_ordered(left, converted, depth + 1)
+
+    # both list
+    left_values = get_values(left)
+    right_values = get_values(right)
+    left_len = len(left_values)
+    right_len = len(right_values)
+    left_shorter = None if left_len == right_len else left_len < right_len
+    while True:
+        try:
+            left_value = left_values.pop(0)
+            right_value = right_values.pop(0)
+        except IndexError:
+            if left_shorter is True:
+                # print(f"{indent}\tLeft side ran out of items, so inputs are in the right order")
+                return left_shorter
+            elif left_shorter is None:
+                return left_shorter
+            else:
+                # print(f"{indent}\tRight side ran out of items, so inputs are not in the right order")
+                return left_shorter
+        result = are_packets_ordered(left_value, right_value, depth + 1)
+        if result is None:
+            continue
+        else:
+            return result
 
 
 @utils.profile
 def part1():
+    # input = example
     input = utils.load_puzzle_input("2022/day13")
     packets = parse_input(input)
     score = 0
-    for ii, (left, right) in enumerate(packets):
-        left = eval(left)
-        right = eval(right)
+    for ii, (left, right) in enumerate(packets, start=1):
         ordered = are_packets_ordered(left, right)
         if ordered:
-            score += ii + 1
+            score += ii
     return score
 
 
+@dataclass
 class Packet:
-    contents: list | int
-
-    def __init__(self, contents):
-        self.contents = contents
+    contents: str
 
     def __lt__(self, other: "Packet") -> bool:
         return are_packets_ordered(self.contents, other.contents)
@@ -100,10 +155,10 @@ def part2():
     input = utils.load_puzzle_input("2022/day13")
     spaces_removed = input.replace("\n\n", "\n")
     spaces_removed += "\n[[2]]\n[[6]]"
-    packets = [Packet(eval(packet)) for packet in spaces_removed.splitlines()]
+    packets = [Packet(packet) for packet in spaces_removed.splitlines()]
     s = [p.contents for p in sorted(packets)]
-    loc2 = s.index([[2]]) + 1
-    loc6 = s.index([[6]]) + 1
+    loc2 = s.index("[[2]]") + 1
+    loc6 = s.index("[[6]]") + 1
     return loc2 * loc6
 
 
