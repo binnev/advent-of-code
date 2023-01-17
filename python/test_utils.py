20973aa1f420ce9c932361422112e0521381cb89
binnev <byntje@gmail.com>
2023-01-17 18:04:45 +0100

    Added testcases for python sparse matrix

--- python/test_utils.py
+++ python/test_utils.py
@@ -15,6 +15,14 @@ from python.utils import SparseMatrix, sparse_matrix_string
             expected=".",
         ),
         param(
+            description="Empty with flip",
+            grid=SparseMatrix(),
+            flip_y=True,
+            pad=0,
+            empty_char=".",
+            expected=".",
+        ),
+        param(
             description="Empty with pad",
             grid=SparseMatrix(),
             flip_y=False,
@@ -29,9 +37,31 @@ from python.utils import SparseMatrix, sparse_matrix_string
             ),
         ),
         param(
+            description="Empty with pad and flip",
+            grid=SparseMatrix(),
+            flip_y=True,
+            pad=1,
+            empty_char=".",
+            expected="\n".join(
+                [
+                    "...",
+                    "...",
+                    "...",
+                ]
+            ),
+        ),
+        param(
             description="Single entry default behaviour",
             grid=SparseMatrix({(0, 0): "A"}),
             flip_y=False,
+            pad=0,
+            empty_char=".",
+            expected="A",
+        ),
+        param(
+            description="Single entry with flip",
+            grid=SparseMatrix({(0, 0): "A"}),
+            flip_y=True,
             pad=0,
             empty_char=".",
             expected="A",
