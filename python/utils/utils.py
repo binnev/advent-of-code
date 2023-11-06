352579a47e681d508e88a7317b098cba47a450d1
binnev <byntje@gmail.com>
2023-11-06 20:50:17 +0100

    Gave _inputs and _solutions folders leading underscores to differentiate them from language folders.

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 
 def load_puzzle_input(filename: str) -> str:
-    path = Path(__file__).parent.parent.parent / f"puzzle_inputs/{filename}.txt"
+    path = Path(__file__).parent.parent.parent / f"_inputs/{filename}.txt"
     with open(path) as file:
         return file.read()
 
 
 def load_solutions(year: str) -> list[list[str]]:
-    path = Path(__file__).parent.parent / f"solutions/{year}.txt"
+    path = Path(__file__).parent.parent.parent / f"_solutions/{year}.txt"
     with open(path) as file:
         contents = file.read()
     lines = contents.split("\n")
