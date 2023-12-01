3b6ee1d11ff2322311461209f19b50683b9f7c2c
binnev <byntje@gmail.com>
2023-12-01 16:24:02 +0100

    Removed sync implementation

--- python/utils/fetch_puzzle_inputs.py
+++ python/utils/fetch_puzzle_inputs.py
@@ -2,9 +2,8 @@ import asyncio
 from datetime import datetime
 from pathlib import Path
 
-from aiofiles import os
 import aiofiles
-import requests
+from aiofiles import os
 from aiohttp import ClientSession
 
 BASE_DIR = Path(__file__).parent.parent.parent
@@ -18,33 +17,10 @@ def main():
     with open(AOC_SESSION_COOKIE_FILE) as file:
         aoc_session_cookie = file.read()
 
-    asyncio.run(_bulk_get_puzzle_inputs_async(aoc_session_cookie))
+    asyncio.run(_get_all_inputs(aoc_session_cookie))
 
 
-def _require_file(path: Path):
-    print(f"Checking {path} exists... ", end="")
-    if not path.exists():
-        raise FileNotFoundError(path.as_posix())
-    print("OK")
-
-
-def _get_puzzle_input(year: int, day: int, cookie: str):
-    target_filename = INPUTS_DIR / f"{year}/day{day}.txt"
-    if target_filename.exists():
-        return
-
-    url = f"https://adventofcode.com/{year}/day/{day}/input"
-    response = requests.get(url=url, cookies={"session": cookie})
-
-    if response.status_code > 200:
-        print("Not available yet; stopping.")
-        exit(1)
-    else:
-        with open(target_filename, "w") as file:
-            file.write(response.text)
-
-
-async def _get_puzzle_input_async(year: int, day: int, session: ClientSession):
+async def _get_puzzle_input(year: int, day: int, session: ClientSession):
     filename = INPUTS_DIR / f"{year}/day{day}.txt"
     if filename.exists():
         print(f"{year=} {day=} already exists; skipping")
@@ -63,7 +39,7 @@ async def _get_puzzle_input_async(year: int, day: int, session: ClientSession):
     print(f"{year=} {day=} fetched; saved to {filename}")
 
 
-async def _bulk_get_puzzle_inputs_async(cookie: str):
+async def _get_all_inputs(cookie: str):
     today = datetime.today()
     this_year = today.year
     this_month = today.month
@@ -84,22 +60,11 @@ async def _bulk_get_puzzle_inputs_async(cookie: str):
     async with ClientSession(cookies={"session": cookie}) as session:
         await asyncio.gather(
             *(
-                _get_puzzle_input_async(year, day, session)
+                _get_puzzle_input(year, day, session)
                 for year, day_range in jobs
                 for day in day_range
             )
         )
-
-
-def _get_all_inputs(cookie: str, this_year: int):
-    print("Fetching puzzle inputs...")
-    for year in range(FIRST_YEAR, this_year + 1):
-        print(f"{year}:", end=" ")
-        (INPUTS_DIR / str(year)).mkdir(exist_ok=True)  # Make sure the folder exists.
-        for day in range(1, 26):
-            _get_puzzle_input(year, day, cookie)
-            print(f"{day}", end=" ")
-        print("")
 
 
 if __name__ == "__main__":
