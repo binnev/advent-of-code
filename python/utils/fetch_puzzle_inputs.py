f69a708b853cb13b1c2cd793a779e84ca061da2e
binnev <byntje@gmail.com>
2023-12-01 14:14:25 +0100

    improved python script

--- python/utils/fetch_puzzle_inputs.py
+++ python/utils/fetch_puzzle_inputs.py
@@ -5,20 +5,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent.parent
 INPUTS_DIR = BASE_DIR / "_inputs"
-assert INPUTS_DIR.exists()
-
 AOC_SESSION_COOKIE_FILE = BASE_DIR / ".aoc-session"
-assert AOC_SESSION_COOKIE_FILE.exists()
-with open(AOC_SESSION_COOKIE_FILE) as file:
-    AOC_SESSION_COOKIE = file.read()
-
-
 FIRST_YEAR = 2015
-THIS_YEAR = datetime.today().year
 
 
 def main():
-    _get_all_inputs()
+    assert INPUTS_DIR.exists()
+    assert AOC_SESSION_COOKIE_FILE.exists()
+    with open(AOC_SESSION_COOKIE_FILE) as file:
+        aoc_session_cookie = file.read()
+
+    this_year = datetime.today().year
+    _get_all_inputs(aoc_session_cookie, this_year)
 
 
 def _require_file(path: Path):
@@ -28,13 +26,13 @@ def _require_file(path: Path):
     print("OK")
 
 
-def _get_puzzle_input(year: int, day: int):
+def _get_puzzle_input(year: int, day: int, cookie: str):
     target_filename = INPUTS_DIR / f"{year}/day{day}.txt"
     if target_filename.exists():
         return
 
     url = f"https://adventofcode.com/{year}/day/{day}/input"
-    response = requests.get(url=url, cookies={"session": AOC_SESSION_COOKIE})
+    response = requests.get(url=url, cookies={"session": cookie})
 
     if response.status_code > 200:
         print("Not available yet; stopping.")
@@ -44,13 +42,13 @@ def _get_puzzle_input(year: int, day: int):
             file.write(response.text)
 
 
-def _get_all_inputs():
+def _get_all_inputs(cookie: str, this_year: int):
     print("Fetching puzzle inputs...")
-    for year in range(FIRST_YEAR, THIS_YEAR + 1):
+    for year in range(FIRST_YEAR, this_year + 1):
         print(f"{year}:", end=" ")
         (INPUTS_DIR / str(year)).mkdir(exist_ok=True)  # Make sure the folder exists.
         for day in range(1, 26):
-            _get_puzzle_input(year, day)
+            _get_puzzle_input(year, day, cookie)
             print(f"{day}", end=" ")
         print("")
 
