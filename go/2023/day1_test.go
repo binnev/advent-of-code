987680826a3e0b70b221796f4ee858cf16cb86a6
binnev <byntje@gmail.com>
2023-12-02 09:54:15 +0100

    Added more tests

--- go/2023/day1_test.go
+++ go/2023/day1_test.go
@@ -24,3 +24,28 @@ func Test_regexMagic(t *testing.T) {
 		})
 	}
 }
+
+var example1 = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+
+var example2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+func TestPart1(t *testing.T) {
+	result := Day1Part1(example1)
+	expected := "142"
+	assert.Equal(t, expected, result)
+}
+
+func TestPart2(t *testing.T) {
+	result := Day1Part2(example2)
+	expected := "281"
+	assert.Equal(t, expected, result)
+}
