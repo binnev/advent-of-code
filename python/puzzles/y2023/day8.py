877a1c881d556c18c2a04c9164af120222ed85a3
binnev <byntje@gmail.com>
2023-12-10 13:35:03 +0100

    Got an iteration checker function working

--- python/puzzles/y2023/day8.py
+++ python/puzzles/y2023/day8.py
@@ -1,36 +1,188 @@
 import re
+
+from typing import Callable
+
 import utils
 
-
-Node = tuple[str, str, str]
+Nodes = dict[str, tuple[str, str]]
+State = tuple[int, str]  # iteration_number, node_id
 
 
 @utils.profile
 def part1(input: str):
-    instructions, nodes = _parse_input(input)
+    instructions, nodes = parse_input(input)
     instructions_len = len(instructions)
     ii = 0
     node_id = "AAA"
     while True:
         if node_id == "ZZZ":
             break
-        left_id, right_id = nodes[node_id]
         instruction = instructions[ii % instructions_len]
-        if instruction == "L":
-            node_id = left_id
-        else:
-            node_id = right_id
-
+        node_id = do_iteration(node_id, instruction, nodes)
         ii += 1
     return ii
 
 
 @utils.profile
 def part2(input: str):
-    ...
+    instructions, nodes = parse_input(input)
+    instructions_len = len(instructions)
+    ii = 0
+    node_ids = [id for id in nodes if id.endswith("A")]
+    while True:
+        if all(id.endswith("Z") for id in node_ids):
+            break
+
+        instruction = instructions[ii % instructions_len]
+        node_ids = [do_iteration(node_id, instruction, nodes) for node_id in node_ids]
+
+        ii += 1
+    return ii
 
 
-def _parse_input(input: str) -> tuple[str, dict[str, tuple[str, str]]]:
+def simulate_trajectory(node_id: str, instructions: str, nodes: Nodes) -> list[State]:
+    wrap = len(instructions)
+    ii = 0
+    output = []
+    while True:
+        output.append((ii, node_id))  # 1 add value
+        if node_id.endswith("Z"):
+            break
+        instruction = instructions[ii % wrap]  # iterate
+        node_id = do_iteration(node_id, instruction, nodes)
+        ii += 1  # update ii
+    return output
+
+
+def calculate_endpoints(
+    node_id: str, n_endpoints: int, instructions: str, nodes: Nodes
+) -> list[int]:
+    """
+    Find out on which iterations we land on an endpoint
+    By calculating based on the loop length
+    """
+    intro, loop = find_cycle(node_id, instructions, nodes)
+
+    # for each endpoint we encountered, find the iteration on which we first landed on it.
+    endpoint_iterations = []
+    for ii, (_, node_id) in enumerate(loop):
+        if node_id.endswith("Z"):
+            endpoint_iterations.append(ii + len(intro))
+
+    result = []
+    n = 0
+    while len(result) < n_endpoints:
+        for endpoint_ii in endpoint_iterations:
+            if len(result) >= n_endpoints:
+                break
+            result.append(endpoint_ii + len(loop) * n)
+        n += 1
+    return result
+
+
+def get_endpoint_checker(node_id: str, instructions: str, nodes: Nodes) -> Callable[[int], bool]:
+    """
+    Generate a function that, for a given trajectory, checks if an iteration number is an endpoint.
+    """
+    intro, loop = find_cycle(node_id, instructions, nodes)
+    intro_len = len(intro)
+    loop_len = len(loop)
+
+    endpoint_iterations = []
+    for ii, (_, node_id) in enumerate(loop):
+        if node_id.endswith("Z"):
+            endpoint_iterations.append(ii + intro_len)
+
+    def checker(iteration: int) -> bool:
+        for endpoint_ii in endpoint_iterations:
+            if iteration == endpoint_ii:
+                return True
+            elif iteration > endpoint_ii:
+                if (iteration - endpoint_ii) % loop_len == 0:
+                    return True
+            else:
+                pass  # ignore ii smaller than the known endpoints
+        return False
+
+    return checker
+
+
+def simulate_endpoints2(
+    node_id: str, n_endpoints: int, instructions: str, nodes: Nodes
+) -> list[int]:
+    results = []
+    ii = 0
+    checker = get_endpoint_checker(node_id, instructions, nodes)
+    while True:
+        if checker(ii):
+            results.append(ii)
+        if len(results) >= n_endpoints:
+            break
+        ii += 1
+    return results
+
+
+def simulate_endpoints(
+    node_id: str, n_endpoints: int, instructions: str, nodes: Nodes
+) -> list[int]:
+    """
+    Find out on which iterations we land on an endpoint
+    By simulating every iteration
+    """
+    wrap = len(instructions)
+    ii = 0
+    endpoints = []
+    while True:
+        if node_id.endswith("Z"):
+            endpoints.append(ii)
+        if len(endpoints) == n_endpoints:
+            break
+
+        instruction = instructions[ii % wrap]  # iterate
+        node_id = do_iteration(node_id, instruction, nodes)
+        ii += 1  # update ii
+    return endpoints
+
+
+def find_cycle(node_id: str, instructions: str, nodes: Nodes) -> tuple[list[State], list[State]]:
+    length = len(instructions)
+    ii = 0
+    # Store state history. State is a combination of instruction index and node_id
+    history = []
+
+    while True:
+        # prepare the state
+        # - which node are we at
+        # - which where are we on the instructions loop?
+        instruction_index = ii % length
+        instruction = instructions[instruction_index]
+        state = (instruction_index, node_id)
+
+        # add state to history (or break)
+        if state in history:
+            break
+        history.append(state)
+
+        # update veriables for the next iteration
+        node_id = do_iteration(node_id, instruction, nodes)
+        ii += 1
+
+    loop_start = history.index(state)
+    intro = history[:loop_start]
+    loop = history[loop_start:]
+    return intro, loop
+
+
+def do_iteration(node_id: str, instruction: str, nodes: Nodes) -> str:
+    left_id, right_id = nodes[node_id]
+    if instruction == "L":
+        node_id = left_id
+    else:
+        node_id = right_id
+    return node_id
+
+
+def parse_input(input: str) -> tuple[str, dict[str, tuple[str, str]]]:
     lines = input.splitlines()
     instructions = lines[0]
     nodes = {}
@@ -39,7 +191,7 @@ def _parse_input(input: str) -> tuple[str, dict[str, tuple[str, str]]]:
         if not line:
             continue
 
-        match = re.match(r"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)", line)
+        match = re.match(r"(\w+) = \((\w+), (\w+)\)", line)
         id, left, right = match.groups()
         nodes[id] = (left, right)
 
@@ -48,4 +200,4 @@ def _parse_input(input: str) -> tuple[str, dict[str, tuple[str, str]]]:
 
 if __name__ == "__main__":
     input = utils.load_puzzle_input("2023/day8")
-    part1(input)
+    part2(input)
--- python/tests/test_2023/test_day8.py
+++ python/tests/test_2023/test_day8.py
@@ -1,4 +1,6 @@
-from puzzles.y2023 import day8
+import pytest
+
+from puzzles.y2023.day8 import *
 
 example1 = """RL
 
@@ -16,18 +18,102 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
 
+example3 = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+
 
 def test_part1():
-    assert day8.part1(example1) == 2
-    assert day8.part1(example2) == 6
+    assert part1(example1) == 2
+    assert part1(example2) == 6
 
 
 def test__parse_input():
-    assert day8._parse_input(example2) == (
+    assert parse_input(example2) == (
         "LLR",
-        [
-            ("AAA", "BBB", "BBB"),
-            ("BBB", "AAA", "ZZZ"),
-            ("ZZZ", "ZZZ", "ZZZ"),
-        ],
+        {
+            "AAA": ("BBB", "BBB"),
+            "BBB": ("AAA", "ZZZ"),
+            "ZZZ": ("ZZZ", "ZZZ"),
+        },
     )
+
+
+def test_part2():
+    assert part2(example3) == 6
+
+
+def test_simulate_endpoints():
+    """
+    If our function to calculate the endpoints is correct, it should match the brute force one
+    that simulates every step.
+    """
+    instructions, nodes = parse_input(example2)
+    simulated = simulate_endpoints("AAA", 10, instructions, nodes)
+    assert len(simulated) == 10
+    calculated = calculate_endpoints("AAA", 10, instructions, nodes)
+    assert len(calculated) == 10
+    simulated2 = simulate_endpoints2("AAA", 10, instructions, nodes)
+    assert len(simulated2) == 10
+    assert simulated == calculated
+    assert simulated == simulated2
+
+
+def test_simulate_trajectory():
+    instructions, nodes = parse_input(example2)
+    steps = simulate_trajectory("AAA", instructions, nodes)
+    assert steps == [
+        (0, "AAA"),
+        (1, "BBB"),
+        (2, "AAA"),
+        (3, "BBB"),
+        (4, "AAA"),
+        (5, "BBB"),
+        (6, "ZZZ"),
+    ]
+
+
+@pytest.mark.parametrize(
+    "start_node, input, expected_intro, expected_loop",
+    [
+        (
+            "11A",
+            example3,
+            [(0, "11A")],
+            [
+                (1, "11B"),
+                (0, "11Z"),
+            ],
+        ),
+        (
+            "22A",
+            example3,
+            [(0, "22A")],
+            [
+                (1, "22B"),
+                (0, "22C"),
+                (1, "22Z"),
+                (0, "22B"),
+                (1, "22C"),
+                (0, "22Z"),
+            ],
+        ),
+    ],
+)
+def test_find_cycle(
+    start_node,
+    input,
+    expected_intro,
+    expected_loop,
+):
+    instructions, nodes = parse_input(input)
+    intro, loop = find_cycle(start_node, instructions, nodes)
+    assert intro == expected_intro
+    assert loop == expected_loop
