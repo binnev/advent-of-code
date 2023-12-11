9fa86b41050dd01d2bf8e7ba27a3a229d68f3dbd
binnev <byntje@gmail.com>
2023-12-11 18:10:49 +0100

    Part 1 working. It was a missing "-" in my regex FML

--- python/puzzles/y2023/day9.py
+++ python/puzzles/y2023/day9.py
@@ -6,7 +6,12 @@ import utils
 @utils.profile
 def part1(input: str):
     sequences = parse_input(input)
-    return sum(predict_next_value(seq) for seq in sequences)
+    result = 0
+    for seq in sequences:
+        next_value = predict_next_value(seq)
+        if next_value is not None:
+            result += next_value
+    return result
 
 
 @utils.profile
@@ -17,7 +22,7 @@ def part2(input: str):
 def parse_input(input: str) -> list[list[int]]:
     result = []
     for line in input.splitlines():
-        result.append(list(map(int, re.findall(r"\d+", line))))
+        result.append(list(map(int, re.findall(r"[\d-]+", line))))
     return result
 
 
@@ -25,20 +30,25 @@ def get_differential(seq: list[int]) -> list[int]:
     return [current - seq[ii - 1] for ii, current in enumerate(seq[1:], start=1)]
 
 
-def predict_next_value(seq: list[int]) -> int:
+def predict_next_value(seq: list[int]) -> int | None:
     """
     The next value is calculated by summing the last value of every differential.
     Once we realise this, we don't actually have to hold on to all the diffs.
+    Return None if the sequence doesn't converge.
     """
     next_value = 0
     diff = seq
+    history = []
     while any(diff):
         next_value += diff[-1]
+        history.append(diff)
         diff = get_differential(diff)
+
+    if len(diff) == 0:
+        return None
     return next_value
 
 
 if __name__ == "__main__":
     input = utils.load_puzzle_input("2023/day9")
-    print(input)
     part1(input)
--- python/tests/test_2023/test_day9.py
+++ python/tests/test_2023/test_day9.py
@@ -1,4 +1,5 @@
 import pytest
+
 from puzzles.y2023.day9 import *
 
 example = """0 3 6 9 12 15
@@ -16,6 +17,17 @@ def test_parse_input():
         [1, 3, 6, 10, 15, 21],
         [10, 13, 16, 21, 30, 45],
     ]
+
+    input = utils.load_puzzle_input("2023/day9")
+    sequences = parse_input(input)
+    assert sequences[0][0] == 17
+    assert sequences[0][-1] == 137
+    assert sequences[5][0] == 2
+    assert sequences[5][1] == -2
+    assert sequences[-1][0] == 14
+    assert sequences[-1][-1] == 549375
+    assert sequences[-3][0] == -5
+    assert sequences[-3][-1] == 13645596
 
 
 @pytest.mark.parametrize(
@@ -46,35 +58,46 @@ def test_get_differential(input, expected):
 @pytest.mark.parametrize(
     "input, expected",
     [
+        ([0, 1, 1, 0], -2),
         ([1, 3, 6, 10, 15, 21], 28),
         ([10, 13, 16, 21, 30, 45], 68),
+        ([1, -4, -9, -14, -19, -24, -29, -34, -39, -44], -49),
         (
             [
-                1,
-                -4,
-                -9,
-                -14,
-                -19,
-                -24,
-                -29,
-                -34,
-                -39,
-                -44,
-                -49,
-                -54,
-                -59,
-                -64,
-                -69,
-                -74,
-                -79,
-                -84,
-                -89,
-                -94,
-                -99,
+                2,
+                2,
+                6,
+                33,
+                127,
+                380,
+                967,
+                2193,
+                4552,
+                8798,
+                16028,
+                27777,
+                46125,
+                73816,
+                114389,
+                172321,
+                253182,
+                363802,
+                512450,
+                709025,
             ],
-            -104,
+            965259,
         ),
     ],
 )
 def test_predict_next_value(input, expected):
     assert predict_next_value(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input",
+    parse_input(utils.load_puzzle_input("2023/day9")),
+)
+def test_predict_next_value2(input):
+    last = input[-1]
+    input = input[:-1]
+    assert predict_next_value(input) == last
