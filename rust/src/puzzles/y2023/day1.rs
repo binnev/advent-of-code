3e7ec4efcd7e4c149b9eb878d2392f81eb14a215
binnev <byntje@gmail.com>
2023-12-14 22:13:18 +0100

    WIP working on part2

--- rust/src/puzzles/y2023/day1.rs
+++ rust/src/puzzles/y2023/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 pub fn part1(input: &str) -> String {
@@ -15,5 +17,35 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    return "".to_string();
+    let digit_map = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+    let mut result = 0;
+    for line in input.lines() {
+        let digit_strings = regex_magic(line);
+        digits = ...;
+        result += ...;
+    }
+    return result.to_string();
+}
+
+fn regex_magic(s: &str) -> Vec<&str> {
+    let rx = Regex::new("one|two|three|four|five|six|seven|eight|nine|[1-9]").unwrap();
+    let matches =Vec::<&str>::from([]);
+    for ii in 0..s.len() {
+        let substr = &s[ii..]; // todo: why do I need to borrow here?
+        match = rx.find(substr); 
+        if match {
+            matches.append(match.group());
+        }
+    }
+    return matches;
 }
