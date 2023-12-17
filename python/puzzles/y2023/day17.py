dd2fdd422f98e2e072b9f4216cb915f57a2dfc58
binnev <byntje@gmail.com>
2023-12-17 14:57:23 +0100

    Trying Dijkstra. But it decides on a node's "shortest distance" too early -- it excludes optimal paths because it refuses to revisit a node

--- python/puzzles/y2023/day17.py
+++ python/puzzles/y2023/day17.py
@@ -1,4 +1,5 @@
 import enum
+import math
 from typing import Callable
 
 import utils
@@ -13,6 +14,11 @@ class Direction(enum.Enum):
 
 
 Path = list[Coord]
+DirectionHistory = tuple[
+    Direction | None,
+    Direction | None,
+    Direction | None,
+]
 
 
 @utils.profile
@@ -22,13 +28,8 @@ def part1(input: str) -> int:
     _, ymax = matrix.get_ylim()
     start = (0, 0)
     finish = (xmax, ymax)
-    scores = dfs(
-        matrix=matrix,
-        path=[start],
-        score=0,
-        finish=finish,
-    )
-    return min(scores)
+    distances, _ = dijkstra(start, finish, matrix)
+    return distances[finish]
 
 
 @utils.profile
@@ -105,6 +106,64 @@ def get_neighbours(coord: Coord, matrix: SparseMatrix) -> list[tuple[Direction, 
 def parse_input(input: str) -> SparseMatrix[Coord, int]:
     matrix = SparseMatrix.from_str(input.strip())
     return SparseMatrix({coord: int(value) for coord, value in matrix.items()})
+
+
+def dijkstra(start: Coord, finish: Coord, matrix: SparseMatrix) -> int:
+    """
+    Calculate the shortest path from start to finish.
+    Don't continue straight for more than 3 iterations.
+    """
+    visited = set()
+    unvisited = set(matrix.keys())
+    min_dist: dict[Coord, int | float] = {coord: math.inf for coord in unvisited}
+    prev_node: dict[Coord, Coord | None] = {coord: None for coord in unvisited}
+    last_3_directions: dict[Coord, DirectionHistory] = {
+        coord: (None, None, None) for coord in unvisited
+    }
+    min_dist[start] = 0
+
+    # todo; do we correctly update distance and everything else if we revisit a node?
+
+    current_node = start
+    while unvisited:
+        # 1. get adjacent nodes (and the direction required to get to them)
+        neighbours = get_neighbours(current_node, matrix)
+        for direction, neighbour in neighbours:
+            already_visited = neighbour in visited
+            dir_hist = last_3_directions[current_node]
+            too_many_straight = all(dirn == direction for dirn in dir_hist)
+            if already_visited or too_many_straight:
+                continue
+            # filter out any visited ones
+            # 2. filter out any 3x straight ones
+            # 3. update min dist
+            neighbour_value = matrix[neighbour]
+            new_dist = min_dist[current_node] + neighbour_value
+            existing_dist = min_dist[neighbour]
+            if new_dist < existing_dist:
+                min_dist[neighbour] = new_dist
+                # 4. update prev node (only if new distance is shorter)
+                prev_node[neighbour] = current_node
+                new_dir_hist = (*last_3_directions[current_node][1:], direction)
+                last_3_directions[neighbour] = new_dir_hist
+
+        # 5. add current node to visited nodes and remove from unvisited
+        visited.add(current_node)
+        unvisited -= visited
+        # 6. choose new current node -- the one with the shortest distance
+        if unvisited:
+            current_node = min(unvisited, key=lambda node: min_dist[node])
+
+    return min_dist, prev_node
+
+
+def get_best_path(prev_node: dict[Coord, Coord], finish: Coord) -> list[Coord]:
+    path = [finish]
+    pp = prev_node[finish]
+    while pp:
+        path.insert(0, pp)
+        pp = prev_node[pp]
+    return path
 
 
 if __name__ == "__main__":
--- python/tests/test_2023/test_day17.py
+++ python/tests/test_2023/test_day17.py
@@ -16,33 +16,68 @@ example = """2413432311323
 2546548887735
 4322674655533"""
 
-small3 = """111
+small3 = """
+111
 222
 333"""
+small3_solution = """
+111
+..2
+..3"""
 
-small4 = """1111
+small4 = """
+1111
 2222
 3333
 4444"""
+small4_solution = """
+1111
+...2
+...3
+...4
+"""
 
-small5 = """11111
+small5 = """
+11111
 22222
 33333
 44444
 55555"""
+small5_solution = """
+1111.
+...22
+....3
+....4
+....5"""
 
-small6 = """111111
+small6 = """
+111111
 222222
 333333
 444444
 555555
-666666"""
+666666
+"""
+small6_solution = """
+1111..
+...22.
+....33
+.....4
+.....5
+.....6
+"""
 
 small4_diagonal = """
 1555
 1155
 5115
 5511
+"""
+small4_diagonal_solution = """
+1...
+11..
+.11.
+..11
 """
 
 small6_nonlinear = """
@@ -53,6 +88,14 @@ small6_nonlinear = """
 888188
 888111
 """
+small6_nonlinear_solution = """
+1..111
+1111.1
+.....1
+...111
+...1..
+...111
+"""
 
 small6_diagonal = """
 155555
@@ -62,20 +105,46 @@ small6_diagonal = """
 555115
 555511
 """
+small6_diagonal_solution = """
+1.....
+11....
+.11...
+..11..
+...11.
+....11
+"""
 
 
 @pytest.mark.parametrize(
-    "input, expected",
+    "input, expected_length, expected_path",
     [
-        (small3, 1 + 1 + 2 + 3),
-        (small4, 1 + 1 + 1 + 2 + 3 + 4),
-        (small5, 1 + 1 + 1 + 2 + 2 + 3 + 4 + 5),
-        (small6, 1 + 1 + 1 + 2 + 2 + 3 + 3 + 4 + 5 + 6),
-        (small4_diagonal, 6),
-        (small6_diagonal, 10),
-        (small6_nonlinear, 16),
-        (example, 102),
+        # (small3, 7, small3_solution),
+        # (small4, 12, small4_solution),
+        # (small5, 19, small5_solution),
+        (small6, 28, small6_solution),
+        (small4_diagonal, 6, small4_diagonal_solution),
+        (small6_diagonal, 10, small6_diagonal_solution),
+        (small6_nonlinear, 16, small6_nonlinear_solution),
+        (example, 102, "example_solution"),
     ],
 )
-def test_part1(input, expected):
-    assert part1(input) == expected
+def test_dijkstra(input, expected_length, expected_path):
+    matrix = parse_input(input)
+    _, xmax = matrix.get_xlim()
+    _, ymax = matrix.get_ylim()
+    start = (0, 0)
+    finish = (xmax, ymax)
+    distances, prev_nodes = dijkstra(start, finish, matrix)
+
+    print("")
+    print("=" * 20)
+    print("for matrix:")
+    matrix.print()
+    print("-" * 20)
+    print("I found the best path:")
+    best_path_nodes = get_best_path(prev_nodes, finish)
+    best_path_matrix = SparseMatrix({node: matrix[node] for node in best_path_nodes})
+    best_path_matrix.print()
+
+    assert distances[finish] == expected_length
+    assert best_path_matrix.to_str().strip() == expected_path.strip()
