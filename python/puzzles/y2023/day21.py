4708a65944469eb2b41e1ea3dd236e145361a1e9
binnev <byntje@gmail.com>
2023-12-21 23:10:05 +0100

    WIP. Not sure this approach is feasible. Certain wall configurations can block the expansion so it's not a perfect diamond.

--- python/puzzles/y2023/day21.py
+++ python/puzzles/y2023/day21.py
@@ -1,5 +1,24 @@
+"""
+NOTES:
+
+It appears to be impossible to completely fill an area with Os. They always alternate on/off.
+
+So what I could do is see if the number of steps is even or odd, and then just bfs outwards from
+the start, and mark each even (or odd) step with Os.
+
+Next step after that: don't set the Os along the way; bfs out and calculate how many squares will
+be covered. Then calculate how many of them will be Os
+
+Can I predict whether a square will be O or . based purely on its coordinates?
+
+"""
+
+
 import utils
 from utils import SparseMatrix, Coord
+
+WALL = "#"
+REACHABLE = "░"
 
 
 @utils.profile
@@ -14,6 +33,57 @@ def part2(input: str):
     pass
 
 
+def hopeful_maths(frontier: set[Coord], matrix: SparseMatrix, steps: int) -> int:
+    """
+    1. With no walls, the number of activated squares after N steps is (N+1)**2
+
+    2. We know where the walls are, we just need to check if they block an active
+    square. A wall blocks an active square if is_odd(taxicab_distance(S, wall)) == is_odd(N)
+
+    3. The number of active squares after N steps is 1. - 2.
+    """
+    start = list(frontier)[0]
+    ideally_reachable_squares = (steps + 1) ** 2
+    walls = {coord: value for coord, value in matrix.items() if value == WALL}
+    walls_that_block = {}
+    for coord, value in walls.items():
+        dist = taxicab_dist(start, coord)
+        oddness_matches = is_odd(dist) == is_odd(steps)
+        inside_diamond = taxicab_dist(coord, start) <= steps
+        if oddness_matches and inside_diamond:
+            walls_that_block[coord] = value
+    return ideally_reachable_squares - len(walls_that_block)
+
+
+def is_odd(n: int) -> bool:
+    return n % 2 != 0
+
+
+def bfs_once(frontier: set[Coord], matrix: SparseMatrix, steps: int) -> int:
+    even_steps = steps % 2 == 0
+    marked = 0
+    if even_steps:
+        marked += len(frontier)
+    visited = frontier
+    for ii in range(1, steps + 1):
+        new_frontier = set()
+        for node in frontier:
+            new_frontier |= get_neighbours(node, matrix)
+        new_frontier -= visited
+
+        frontier = new_frontier
+
+        even_ii = ii % 2 == 0
+        if (even_steps and even_ii) or (not even_steps and not even_ii):
+            marked += len(frontier)
+
+    return marked
+
+
+def forgetful_bfs_summary(frontier: set[Coord], matrix: SparseMatrix, steps: int) -> int:
+    return len(forgetful_bfs(frontier, matrix, steps))
+
+
 def forgetful_bfs(frontier: set[Coord], matrix: SparseMatrix, steps: int) -> set[Coord]:
     """
     This will be just like bfs except we don't maintain a "visited" set, and we can revisit squares.
@@ -22,7 +92,6 @@ def forgetful_bfs(frontier: set[Coord], matrix: SparseMatrix, steps: int) -> set
         new_frontier = set()
         for node in frontier:
             new_frontier |= get_neighbours(node, matrix)
-
         frontier = new_frontier
     return frontier
 
@@ -40,11 +109,20 @@ def get_neighbours(coord: Coord, matrix: SparseMatrix) -> set[Coord]:
 
 def parse_input(input: str) -> tuple[SparseMatrix, Coord]:
     matrix = SparseMatrix.from_str(input.strip())
+    # matrix = SparseMatrix(
+    #     {coord: (WALL if value == "#" else value) for coord, value in matrix.items()}
+    # )
     start = next(coord for coord, value in matrix.items() if value == "S")
     matrix[start] = "."
     return matrix, start
 
 
+def taxicab_dist(a: Coord, b: Coord) -> int:
+    dx = abs(a[0] - b[0])
+    dy = abs(a[1] - b[1])
+    return dx + dy
+
+
 if __name__ == "__main__":
     input = utils.load_puzzle_input("2023/day21")
-    part1(input)
+    print(input)
--- python/tests/test_2023/test_day21.py
+++ python/tests/test_2023/test_day21.py
@@ -51,6 +51,25 @@ example_after3 = """...........
 ..........."""
 
 
+my_example = """
+#############
+#...........#
+#.#########.#
+#S#########.#
+#...#######.#
+###.#######.#
+#...........#
+#############
+"""
+my_example = """
+.......
+.......
+...S...
+.......
+.......
+"""
+
+
 @pytest.mark.parametrize(
     "steps, expected",
     [
@@ -66,7 +85,94 @@ def test_forgetful_bfs_visual(steps, expected):
     assert visual.to_str() == expected
 
 
-def test_forgetful_bfs_part1():
+# @pytest.mark.parametrize(
+#     "steps, expected",
+#     [
+#         (6, 16),
+#         (10, 50),
+#         (50, 1594),
+#         (100, 6536),
+#         # (500, 167004),
+#         # (1000, 668697),
+#         # (5000, 16733044),
+#         # (26501365, ...),  # too slow
+#     ],
+# )
+# def test_forgetful_bfs_infinite(steps, expected):
+#     matrix, start = parse_input(example)
+#     reachable = forgetful_bfs_summary({start}, matrix, steps)
+#     assert reachable == expected
+
+
+@pytest.mark.parametrize(
+    "steps, expected",
+    [
+        # (1, 2),
+        (2, 4),
+        (3, 6),
+        (4, 9),
+        (5, 13),
+        (6, 16),
+    ],
+)
+@pytest.mark.parametrize(
+    "algo",
+    [
+        # forgetful_bfs_summary,
+        hopeful_maths,
+        bfs_once,
+    ],
+)
+def test_finite_example(algo, steps, expected):
     matrix, start = parse_input(example)
-    reachable = forgetful_bfs({start}, matrix, 6)
-    assert len(reachable) == 16
+    reachable = algo({start}, matrix, steps)
+    assert reachable == expected
+
+
+# @pytest.mark.parametrize(
+#     "steps, expected",
+#     [
+#         (50, 2222),
+#         (75, 4846),
+#         (100, 6714),
+#         (200, 7521),
+#     ],
+# )
+@pytest.mark.parametrize(
+    "algo",
+    [
+        forgetful_bfs_summary,
+        hopeful_maths,
+        bfs_once,
+    ],
+)
+def test_finite_input_algos(algo):
+    steps = [50, 75, 100, 200]
+    expected = [2222, 4846, 6714, 7521]
+    matrix, start = parse_input(utils.load_puzzle_input("2023/day21"))
+    results = [algo({start}, matrix, s) for s in steps]
+    assert results == expected
+
+
+def test_diamond_expands_forever():
+    matrix, start = parse_input(my_example)
+    frontier = {start}
+    steps = 10
+    for _ in range(steps):
+        print("----------------")
+        frontier = forgetful_bfs(frontier, matrix, 1)
+        visual = SparseMatrix({**matrix, start: "S", **{coord: "O" for coord in frontier}})
+        visual.print()
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        ((5, 5), (6, 5), 1),
+        ((6, 5), (5, 5), 1),
+        ((0, 0), (0, 1), 1),
+        ((0, 0), (0, -1), 1),
+    ],
+)
+def test_taxicab_dist(a, b, expected):
+    assert taxicab_dist(a, b) == expected
