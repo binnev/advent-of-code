73ae1661bad75d16ab93e4785fc3fef9df8e172f
binnev <byntje@gmail.com>
2023-12-24 10:40:45 +0100

    Fixed go

--- go/utils/loading.go
+++ go/utils/loading.go
@@ -14,14 +14,14 @@ func LoadPuzzleInput(filename string) string {
 	// using a relative path results in different results when running the tests
 	// and running main.go
 	_, current_file, _, _ := runtime.Caller(0)
-	relative_file_path := fmt.Sprintf("../../../_inputs/%v.txt", filename)
+	relative_file_path := fmt.Sprintf("../../../.puzzle-inputs/%v.txt", filename)
 	file_path := path.Join(current_file, relative_file_path)
 
 	data, err := os.ReadFile(file_path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(data)
+	return strings.TrimSpace(string(data))
 }
 
 func LoadSolutions(year string) [][]string {
