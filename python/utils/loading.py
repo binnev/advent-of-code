56b8c98485b10955f0372132a0836f7b10c3363a
binnev <byntje@gmail.com>
2023-12-24 10:44:55 +0100

    Fixed python loader

--- python/utils/loading.py
+++ python/utils/loading.py
@@ -4,4 +4,4 @@ from utils.fetch_puzzle_inputs import INPUTS_DIR
 def load_puzzle_input(filename: str) -> str:
     path = INPUTS_DIR / f"{filename}.txt"
     with open(path) as file:
-        return file.read()
+        return file.read().strip()
