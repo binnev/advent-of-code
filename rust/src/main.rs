32e2e7bccbe5baacef0791f916e0bebc0a6d8e76
binnev <robin.m.neville@gmail.com>
2025-01-26 22:26:00 +0100

    day19 part 2 working on examples but too slow

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,6 +6,6 @@ mod utils;
 
 fn main() {
     let input = crate::utils::load_puzzle_input("2024/day19");
-    let result = crate::puzzles::y2024::day19::part1(&input);
+    let result = crate::puzzles::y2024::day19::part2(&input);
     println!("{result}");
 }
--- rust/src/puzzles/y2024/day19.rs
+++ rust/src/puzzles/y2024/day19.rs
@@ -11,7 +11,13 @@ pub fn part1(input: &str) -> usize {
     out
 }
 pub fn part2(input: &str) -> usize {
-    0
+    let (available_parts, desired_patterns) = parse(input).unwrap();
+    let mut out = 0;
+    for desired in desired_patterns {
+        let solutions = get_solutions(desired, &available_parts);
+        out += solutions.len();
+    }
+    out
 }
 fn has_solutions(desired: &str, available_parts: &Vec<&str>) -> bool {
     let mut options = HashSet::from([desired]);
@@ -41,28 +47,91 @@ fn has_solutions(desired: &str, available_parts: &Vec<&str>) -> bool {
 }
 /// Find the number of ways the desired pattern can be constructed from the
 /// available parts.
-fn get_num_solutions(desired: &str, available_parts: &Vec<&str>) -> usize {
-    // Base case: there is nothing left to construct, so we were able to
-    // construct the desired pattern
-    if desired.len() == 0 {
-        return 1;
-    }
-
-    // Recursive case: there are letters left in the desired pattern. There may
-    // be multiple parts that match the start of the pattern. Loop over the
-    // available parts and see which ones match the start of the desired
-    // pattern. For those that match, subtract them from the desired pattern
-    // and investigate recursively
-    let matching_parts: Vec<_> = available_parts
-        .into_iter()
-        .filter(|&part| desired.starts_with(part))
-        .collect();
-    let mut solutions = 0;
-    for part in matching_parts {
-        let trimmed = desired.trim_start_matches(part);
-        solutions += get_num_solutions(trimmed, available_parts);
+///
+/// This works by iterating over the available parts and trying to subtract them
+/// from the start of the desired pattern. This continues until either
+/// - no parts can be subtracted (fail)
+/// - the whole desired pattern is consumed (success)
+///
+/// Because multiple parts could match at each stage, we keep a hashset of all
+/// the viable solutions. The hashset enforces uniqueness which keeps this
+/// relatively quick.
+///
+/// OK so the hashset does not keep it quick at all.
+/// Ideas to make this better:
+/// - Use a Trie?
+///
+/// Instead of storing all the parts that make a solution, store the
+/// combination of parts as a string, along with the _number of ways it can be
+/// created_.
+///
+/// So instead of:
+///
+/// {
+///     ["w", "w", "gg"],
+///     ["ww", "gg"],
+/// }
+///
+/// do
+///
+/// {
+///     "wwgg": 2
+/// }
+///
+/// We don't need to actually give the solution parts, we just need the solution
+/// _count_.
+fn get_solutions<'a, 'd>(
+    desired: &'d str,
+    available_parts: &Vec<&'a str>,
+) -> HashSet<Vec<&'a str>> {
+    println!("\nFinding solutions for {desired}\n");
+    let start = PotentialSolution {
+        remainder: desired,
+        parts:     vec![],
+    };
+    let mut potential_solutions = HashSet::from([start]);
+    let mut solutions = HashSet::new(); // completed solutions
+    while potential_solutions.len() > 0 {
+        println!("potential solutions:");
+        for s in potential_solutions.iter() {
+            println!("{:?}", s.parts);
+        }
+        if potential_solutions.len() > 10 {
+            break;
+        }
+        let mut continue_investigating = HashSet::new();
+        for solution in potential_solutions.iter() {
+            for part in available_parts {
+                // If one of the available parts can be subtracted from the
+                // start of the desired pattern
+                if let Some(trimmed) = solution.remainder.strip_prefix(part) {
+                    let mut new_solution = solution.clone();
+                    new_solution.remainder = trimmed;
+                    new_solution.parts.push(part);
+                    // If we've found a complete solution, add it to the output.
+                    // If there is still some of the pattern left, continue
+                    // investigating.
+                    if trimmed.len() == 0 {
+                        solutions.insert(new_solution);
+                    } else {
+                        continue_investigating.insert(new_solution);
+                    }
+                }
+            }
+            // If we get here then none of the parts matched, so the solution is
+            // not viable and it is dropped.
+        }
+        potential_solutions = continue_investigating;
     }
     solutions
+        .into_iter()
+        .map(|solution| solution.parts)
+        .collect()
+}
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct PotentialSolution<'a, 'd> {
+    remainder: &'d str,
+    parts:     Vec<&'a str>,
 }
 fn parse(input: &str) -> Option<(Vec<&str>, Vec<&str>)> {
     let mut parts = input.split("\n\n");
@@ -79,15 +148,18 @@ mod tests {
     }
     #[test]
     fn test_part2() {
-        assert_eq!(part2(EXAMPLE), 0);
+        assert_eq!(part2(EXAMPLE), 16);
     }
 
     #[test]
-    fn test_get_num_solutions() {
+    fn test_get_solutions() {
         let desired = "brr";
         let available_parts = vec!["br", "b", "r"];
         // [br, r] and [b, r, r] are both possible.
-        assert_eq!(get_num_solutions(desired, &available_parts), 2,);
+        assert_eq!(
+            get_solutions(desired, &available_parts),
+            HashSet::from([vec!["br", "r"], vec!["b", "r", "r"],]),
+        );
     }
 }
 const EXAMPLE: &str = "r, wr, b, g, bwu, rb, gb, br
