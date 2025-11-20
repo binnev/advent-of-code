2e1bbc404d20dc9e6264dddb8e7de1df9bc32f04
binnev <robin.m.neville@gmail.com>
2025-11-20 16:42:40 +0100

    update input fetching shell script

--- fetch_puzzle_input.sh
+++ fetch_puzzle_input.sh
@@ -1,1 +1,7 @@
-curl https://adventofcode.com/$1/day/$2/input --header "Cookie: session=$(cat .aoc-session)" > .puzzle-inputs/$1/day$2.txt
+set -e  # fail on errors
+DIR=".puzzle-inputs/$1"
+curl -O https://adventofcode.com/$1/day/$2/input \
+    --header "Cookie: session=$(cat .aoc-session)" \
+    --fail
+mkdir -p $DIR
+mv input $DIR/day$2.txt
