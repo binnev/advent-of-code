062e61984f9b03043639bcbf67f7a2600fc44163
binnev <robin.m.neville@gmail.com>
2025-11-20 23:23:47 +0100

    update python script to fetch puzzle inputs

--- python/utils/fetch_puzzle_inputs.py
+++ python/utils/fetch_puzzle_inputs.py
@@ -1,74 +1,86 @@
 import asyncio
-from datetime import datetime
+from asyncio import BoundedSemaphore
+from datetime import date
 from pathlib import Path
+from typing import Awaitable
 
 import aiofiles
-from aiofiles import os
-from aiohttp import ClientSession
+from httpx import AsyncClient
 
 BASE_DIR = Path(__file__).parent.parent.parent
 INPUTS_DIR = BASE_DIR / ".puzzle-inputs"
 AOC_SESSION_COOKIE_FILE = BASE_DIR / ".aoc-session"
 FIRST_YEAR = 2015
+MAX_CONCURRENT_REQUESTS = 3
+
+Year = int
+Day = int
+Job = tuple[Year, Day]
 
 
-def main():
+async def main() -> None:
+    """Fetch all the puzzle inputs."""
     assert AOC_SESSION_COOKIE_FILE.exists()
     with open(AOC_SESSION_COOKIE_FILE) as file:
         aoc_session_cookie = file.read().strip()
 
-    asyncio.run(_get_all_inputs(aoc_session_cookie))
+    client = AsyncClient(headers={"Cookie": f"session={aoc_session_cookie}"})
+    sem = BoundedSemaphore(MAX_CONCURRENT_REQUESTS)
+    jobs = _generate_years_and_days()
+    coros = [_get_puzzle_input(year, day, client) for year, day in jobs]
+    await asyncio.gather(*(rate_limited(coro, sem) for coro in coros))
 
 
-async def _get_puzzle_input(year: int, day: int, session: ClientSession):
+async def rate_limited[T](coro: Awaitable[T], sem: BoundedSemaphore) -> T:
+    async with sem:
+        return await coro
+
+
+async def _get_puzzle_input(year: int, day: int, client: AsyncClient) -> None:
+    """Fetch 1 puzzle input and save it to file."""
+
+    prefix = f"{year=} {day=}"
     filename = INPUTS_DIR / f"{year}/day{day}.txt"
     if filename.exists():
-        print(f"{year=} {day=} already exists; skipping")
+        print(f"{prefix} exists; skipping")
         return
 
     url = f"https://adventofcode.com/{year}/day/{day}/input"
-    response = await session.request(method="GET", url=url)
-    text = await response.text()
-    if not response.ok:
-        print(f"{year=} {day=} is not available yet: {text}")
+    print(f"{year=} {day=} fetching...")
+    response = await client.get(url)
+    text = response.text
+    if response.is_error:
+        print(f"{year=} {day=} {response.status_code} ERROR: {text}")
         return
 
-    async with aiofiles.open(filename, mode="w") as file:
+    # Only create the file if the request was successful
+    filename.parent.mkdir(exist_ok=True, parents=True)
+    async with aiofiles.open(filename, "w") as file:
         await file.write(text)
     print(f"{year=} {day=} fetched; saved to {filename}")
 
 
-async def _get_all_inputs(cookie: str):
-    today = datetime.today()
+def _generate_years_and_days() -> list[tuple[Year, Day]]:
+    today = date.today()
     this_year = today.year
     this_month = today.month
     this_day = today.day
     is_december = this_month == 12
 
-    # only consider the current year if it is december
+    # only consider the current year if it is December. Otherwise we'll get lots
+    # of unnecessary failures for unreleased puzzles.
     years = range(FIRST_YEAR, this_year + 1 if is_december else this_year)
-    jobs = []
+    jobs: list[tuple[Year, Day]] = []
     for year in years:
-        # for the current year, only get up to today's puzzle. For previous years, get them all
+        # for the current year, only get up to today's puzzle.
+        # For previous years, get them all
         max_day = this_day + 1 if year == this_year else 26
-        jobs.append((year, range(1, max_day)))
+        for day in range(1, max_day):
+            jobs.append((year, day))
 
-    # make sure all the year dirs exist first
-    await asyncio.gather(
-        *(os.makedirs(INPUTS_DIR / str(year), exist_ok=True) for year in years)
-    )
-    # fetch the inputs
-    async with ClientSession(
-        headers={"Cookie": f"session={cookie}"}
-    ) as session:
-        await asyncio.gather(
-            *(
-                _get_puzzle_input(year, day, session)
-                for year, day_range in jobs
-                for day in day_range
-            )
-        )
+    # Reverse the jobs so we download the most recent day first
+    return list(reversed(jobs))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
